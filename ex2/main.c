7910aa9ac229274a868d6841fc236df54346a2ff
Zhuyue0324 <zhuyue0324@sina.com>
2018-05-11 17:39:39 +0200

    Add files via upload

--- ex2/main.c
+++ ex2/main.c
@@ -40,7 +40,10 @@
  *   iteration loop the operation sor() is used.
  * - calculate_uv() Calculate the velocity at the next time step.
  */
-int main(int argn, char** args){//.sim karman/step/natural/trap/rbc
+int main(int argn, char** args){//.sim karman/step/natural1/natural2/trap/rbc
+  
+  //The initial variables read from the .dat files, together with their pointers
+
   double valRe=0;
   double valUI=0;
   double valVI=0;
@@ -83,36 +86,54 @@ int main(int argn, char** args){//.sim karman/step/natural/trap/rbc
   double *eps=&valeps;
 	double *dt_value=&valdt_value;
 
-  //I find it really complicated to generate file names with the input words using c because of the pointers...
+  double valINUI=0.0;
+  double valINVI=0.0;
+  double *INUI=&valINUI;
+  double *INVI=&valINVI;
+  //The equivalent default value for a non Heat problem so that they can use the same equations
+  double valTi=0.0;
+  double valBeta=0.0;
+  double valPr=1.0;
+  double *Ti=&valTi;
+  double *beta=&valBeta;
+  double *Pr=&valPr;
+
+  //To generate filename. problem=args[1].dat, geometry=args[1].pgm, outputfilename=output/args[1]
+  //P.S. I find it really painful to generate file names with the input words using c because of the pointers...
 
   char Filename1[(int)(sizeof(args[1])/sizeof(args[1][0]))];
   char Filename2[(int)(sizeof(args[1])/sizeof(args[1][0]))];
   char Filename3[(int)(sizeof(args[1])/sizeof(args[1][0]))];
   char *foldername="output/";
+  char *aftername1=".dat";
+  char *aftername2=".pgm";
   char Outputfilename[(int)(sizeof(args[1])/sizeof(args[1][0]))+7];
+  char problem[(int)(sizeof(args[1])/sizeof(args[1][0]))+4];
+  char geometry[(int)(sizeof(args[1])/sizeof(args[1][0]))+4];
   strcpy(Filename1,args[1]);
   strcpy(Filename2,args[1]);
   strcpy(Filename3,args[1]);
-  char* problem=strcat(Filename1,".dat");
-  char* geometry=strcat(Filename2,".pgm");
+  //char* problem=strcat(Filename1,".dat");
+  //char* geometry=strcat(Filename2,".pgm");
   for (int i=0;i<7;i++){
     Outputfilename[i]=foldername[i];
   }
   for (int i=0;i<(int)(sizeof(args[1])/sizeof(args[1][0]));i++){
     Outputfilename[i+7]=Filename3[i];
+    problem[i]=Filename1[i];
+    geometry[i]=Filename2[i];
+  }
+  for (int i=0;i<4;i++){
+    problem[(int)(sizeof(args[1])/sizeof(args[1][0]))+i]=aftername1[i];
+    geometry[(int)(sizeof(args[1])/sizeof(args[1][0]))+i]=aftername2[i];
   }
 
-
-
-  double valINUI=0.0;
-  double valINVI=0.0;
-  double *INUI=&valINUI;
-  double *INVI=&valINVI;
+  //Set flags for the question: is this a fluid or heat problem? is it karman or step or natural convection or heat trap or rbc?
 
   double Th=1.0;
   double Tc=0.0;
-  int TypeQuestion=0;
-  int NameQuestion=0;
+  int TypeQuestion=0;//1=fluid, 2=heat
+  int NameQuestion=0;//1=karman, 2=step, 3=natural convection, 4=trap, 5=rbc
   if (strcmp(args[1],"karman")==0){
     NameQuestion=1;
     TypeQuestion=1;//fluid
@@ -121,7 +142,13 @@ int main(int argn, char** args){//.sim karman/step/natural/trap/rbc
     NameQuestion=2;
     TypeQuestion=1;
   }
-  else if (strcmp(args[1],"natural")==0){
+  else if (strcmp(args[1],"natural1")==0){
+    NameQuestion=3;
+    TypeQuestion=2;//heat
+    Th=1.0;
+    Tc=0.0;
+  }
+  else if (strcmp(args[1],"natural2")==0){
     NameQuestion=3;
     TypeQuestion=2;//heat
     Th=1.0;
@@ -140,25 +167,12 @@ int main(int argn, char** args){//.sim karman/step/natural/trap/rbc
     Tc=291.20;
   }
 
-  //The equivalent default value for a non Heat problem so that they can use the same equations
-  double valTi=0.0;
-  double valBeta=0.0;
-  double valPr=1.0;
-
-  double *Ti=&valTi;
-  double *beta=&valBeta;
-  double *Pr=&valPr;
+  //read parameters from args[1].dat
   
   read_parameters(problem, Re,UI,VI,PI,GX,GY,t_end,xlength,ylength,dt,dx,dy,imax,jmax,alpha,omg,tau,itermax,eps,dt_value,TypeQuestion, Ti, Pr, beta, INUI, INVI);
-  //Q4 for changing omega
-  //*omg=2.0;
-  //Q6 for U tends to infinity
-  //*imax=512;
-  //*jmax=512;
-  //*t_end=500;
-  //Q7 for changing Re
-  //*Re=10000;
-  
+    
+  //reserve memory for the global matrices
+
   double **U = matrix ( 0 , *imax , 0 , *jmax+1 );
   double **V = matrix ( 0 , *imax+1 , 0 , *jmax );
   double **P = matrix ( 0 , *imax+1 , 0 , *jmax+1 );
@@ -176,6 +190,8 @@ int main(int argn, char** args){//.sim karman/step/natural/trap/rbc
   double **Geo=matrix(0,*imax+1,0,*jmax+1);
   init_matrix(Geo, 0,*imax+1,0,*jmax+1, 0.0);
 
+  //set global variables  to compute timestamps, residual of sor, and restTime variable judges when to print out a vtk file
+
   double valt=0;
   int valn=0;
   int valit=0;
@@ -187,8 +203,17 @@ int main(int argn, char** args){//.sim karman/step/natural/trap/rbc
   int *it=&valit;
   double *res= &valres;
   int solbeh=0;
+
+  //initialize U,V,P, and T
+
   init_uvp(*UI,*VI,*PI,*imax,*jmax,U,V,P, TypeQuestion, *Ti, Temp);
+
+  //initialize flags together with its geometry structure (Geo, can be print out later)
+
   init_flag(problem,geometry, *imax, *jmax, Flag, Geo);
+
+  //Compute the number of fluid cells in the problem, can be used in the sor iteration as a global variable
+
   double NumberFluidCell=0.0;
   for (int i=1;i<=*imax;i++){
     for (int j=1;j<=*jmax;j++){
@@ -198,25 +223,54 @@ int main(int argn, char** args){//.sim karman/step/natural/trap/rbc
     }
   }
 
+  //write a vtk file at the beginning
 
-  //boundaryvalues(*imax, *jmax, U, V, Flag, NameQuestion, Th, Tc, Temp, *INUI, *INVI);
-  //write_vtkFile("output/output", *n, *xlength, *ylength, *imax, *jmax, *dx, *dy, U, V, P);
+  boundaryvalues(*imax, *jmax, U, V, Flag, NameQuestion, Th, Tc, Temp, *INUI, *INVI);
+  if (TypeQuestion==1){
+    write_vtkFile(Outputfilename, *n, *xlength, *ylength, *imax, *jmax, *dx, *dy, U, V, Geo);
+  }
+  else if (TypeQuestion==2){
+    write_vtkFile(Outputfilename, *n, *xlength, *ylength, *imax, *jmax, *dx, *dy, U, V, Temp);
+  }
+
+  //main loop
 
   while (*t<*t_end){
+
+    //compute dt
+
     if (*tau<=0){
       *dt=*dt_value;//0.005;
     }
     else{
       calculate_dt(*Re, *tau, dt, *dx, *dy, *imax, *jmax, U, V, Flag,*Pr);
     }
+
+    //compute boundary for U,V,T
+
     boundaryvalues(*imax, *jmax, U, V, Flag, NameQuestion, Th, Tc, Temp, *INUI, *INVI);
+
+    //compute T^(n+1) if this is a heat question
+
     if (TypeQuestion==2){
       calculate_temp(*Re, *Pr, *dt, *dx, *dy, *imax, *jmax, U, V, Temp, *alpha);
     }
+
+    //compute F, G
+
     calculate_fg(*Re, *GX, *GY, *alpha, *dt, *dx, *dy, *imax, *jmax, U, V, F, G, Flag, TypeQuestion,*beta, Temp);
+
+    //compute right hand side
+
     calculate_rs(*dt, *dx, *dy, *imax, *jmax, F, G, RS, Flag);
+
+    //reinitialize the counter of iteration for the sor solver
+
     *it=0;
     *res=*eps+1;
+
+    //compute sor, either converge or not converge but arrive at itermax
+
     while ((*it<*itermax) && (*res>*eps)){
       sor(*omg, *dx, *dy, *imax, *jmax, P, RS, res, Flag,NumberFluidCell);
       *it=*it+1;
@@ -225,29 +279,30 @@ int main(int argn, char** args){//.sim karman/step/natural/trap/rbc
     if (*it==*itermax){
       printf("WARNING! The SOR have not converged in this iteration!\n");
     }
+
+    //compute new U, V
+
     calculate_uv(*dt, *dx, *dy, *imax, *jmax, U, V, F, G, P, Flag);
-    //Q5 help check if stable
-    /*for (int i=1;i<=*imax;i++){
-      for (int j=1;j<=*jmax;j++){
-        if ((isnan(U[i][j]))||(isnan(V[i][j]))||(isnan(F[i][j]))||(isnan(G[i][j]))||(isnan(P[i][j]))||(isnan(RS[i][j]))){
-          printf("Unstable, exist NaN in U,V,P,F,G or RS \n");
-          free_matrix (U, 0 , *imax , 0 , *jmax+1 );
-          free_matrix (V, 0 , *imax+1 , 0 , *jmax );
-          free_matrix (P, 0 , *imax+1 , 0 , *jmax+1 );
-          free_matrix (RS, 1 , *imax , 1 , *jmax );
-          free_matrix (F, 0 , *imax , 1 , *jmax );
-          free_matrix (G, 1 , *imax , 0 , *jmax );
-          return 0;
-        }
-      }
-    }*/
+    
+    
     *t+=*dt;
     restTime+=*dt; 
     *n=*n+1;
+
+    //write a vtk file after a dt_value
+
     if (restTime>=*dt_value){
-      //write_vtkFile("output/output", *n, *xlength, *ylength, *imax, *jmax, *dx, *dy, U, V, P);
+      if (TypeQuestion==1){
+        write_vtkFile(Outputfilename, *n, *xlength, *ylength, *imax, *jmax, *dx, *dy, U, V, Geo);
+      }
+      else if (TypeQuestion==2){
+        write_vtkFile(Outputfilename, *n, *xlength, *ylength, *imax, *jmax, *dx, *dy, U, V, Temp);
+      }
       restTime=restTime-*dt_value;
     }
+
+    //print out n, dt, t, #sor, Residual in the terminal
+
     printf("n=");
     printf("%i",*n);
     printf(", dt=");
@@ -258,10 +313,10 @@ int main(int argn, char** args){//.sim karman/step/natural/trap/rbc
     printf("%i",*it);
     printf(", Res=");
     printf("%f\n",*res);
-    //printf(", U[imax/2][7*jmax/8]=");
-    //printf("%f \n",U[*imax/2][(int)(((double)*jmax)/8.0*7.0)]);
   }
-  printf("Finish while loop\n");
+
+  //write out the final file
+  
   if (TypeQuestion==1){
     write_vtkFile(Outputfilename, *n, *xlength, *ylength, *imax, *jmax, *dx, *dy, U, V, Geo);
   }
@@ -269,8 +324,13 @@ int main(int argn, char** args){//.sim karman/step/natural/trap/rbc
     write_vtkFile(Outputfilename, *n, *xlength, *ylength, *imax, *jmax, *dx, *dy, U, V, Temp);
   }
   
+  //print how many iterations in sor in all 
+
   printf("Total #SOR=");
   printf("%i \n", solbeh);
+
+  //free memory space
+
   free_matrix (U, 0 , *imax , 0 , *jmax+1 );
   free_matrix (V, 0 , *imax+1 , 0 , *jmax );
   free_matrix (P, 0 , *imax+1 , 0 , *jmax+1 );
